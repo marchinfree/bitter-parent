d60409d33adda421d183f2de3ed59c62866c339a
Jessica Chin <jntchin@gmail.com>
2019-09-12 21:02:15 -0400

    made form component with axios calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import axios from 'axios';
+import Form from './Form'
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 class App extends Component {
   constructor(){
@@ -13,107 +14,17 @@ class App extends Component {
       healthyOptionsArray: [],
     }
   }
-
-
-  // healthyFunction = () => {
-  //     Math.floor(Math.random()*100)+1;
-  // }
-
-
-  componentDidMount(){
-  //this shows we can make an axios call that returns the food in a way that we want
-    axios({
-      url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-      dataResponse: 'JSON',
-      method: 'POST',
-      data: {
-        "query": "chocolate bar",
-      },
-      headers: {
-        'x-app-id': '4424dc15',
-        'x-app-key': 'adf9b4e2b35bea41e8e10c775b249104',
-        // 'content-type': 'application/x-www-form-urlencoded',
-        // 'x-remote-user-id': 0,
-      },
-    }).then((response) => {
-      //this shows we can narrow down the response from the API call
-      // const foodGroup = response.data.foods[0].tags.food_group;
-      // console.log(response.data.foods[0].tags.food_group);
-      //this works when we set it as a variable, but not when we try to push it into state.
-      this.setState({
-        junkFood: response.data.foods[0],
-        junkFoodSugar: response.data.foods[0].nf_sugars
-      })
-    }).then(()=>{
-      axios({
-        url: 'https://trackapi.nutritionix.com/v2/search/instant/',
-        dataResponse: 'JSON',
-        method: 'POST',
-        data: {
-          "query": "chocolate bar",
-          "detailed": true,
-          "common": true,
-          "branded": false,
-          "full_nutrients": {
-            "269": {
-              "lte": (this.state.junkFoodSugar) - 10
-            },
-          }
-        },
-        headers: {
-          'x-app-id': '4424dc15',
-          'x-app-key': 'adf9b4e2b35bea41e8e10c775b249104',
-        }
-      }) .then((results)=>{
-        console.log(results);
-        this.setState({
-          healthyOptionsArray: results.data.common,
-          healthyFood: Math.floor(Math.random()*100)+1,
-        })
-      }) .then(()=>{
-        // console.log(this.state.healthyOptionsArray.length);
-        console.log(this.state.healthyFood);
-      })
-    })
-    
-
-
-      
-
-
-
-    
-  //   .then(() =>{
-  //     axios({
-  //       url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-  //       dataResponse: 'JSON',
-  //       method: 'POST',
-  //       data: {
-  //         "query": "cheese",
-  //       },
-  //       headers: {
-  //         'x-app-id': '4424dc15',
-  //         'x-app-key': 'adf9b4e2b35bea41e8e10c775b249104',
-  //       },
-  //     }).then((result) => {
-  //       console.log(result.data.foods[0].tags.food_group);
-  //       this.setState({
-  //         healthyFood: result.data.foods[0],
-  //       })
-  //     })
-  // })
-  
-  }
-
   
 
   render(){
+    return (
+      <div className="App">
+        
+        <Form junkFood={this.state.junkFood} healthyFood={this.state.healthyFood} junkFoodSugar={this.state.junkFoodSugar} healthySugar={this.state.healthySugar} healthyOptionsArray={this.state.healthyOptionsArray} />
 
-  return (
-    <div className="App">
-      <p>You are eating {this.state.junkFood.nf_sugars} grams of sugar</p>
-    </div>
-  );
+        <p>You are eating {this.state.junkFood.nf_sugars} grams of sugar</p>
+      </div>
+    );
   }
 }
 
