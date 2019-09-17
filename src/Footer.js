dd866796f5a140f7beb2a868717449637de1af63
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-17 19:45:07 -0400

    fixed seb name

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () =>{
     return (
         <footer>
-            <p>&copy; Jessica Chin, Sebastian Freeman and Mohamed Omar 2019</p>
+            <p>&copy; Jessica Chin, Seb Freeman and Mohamed Omar 2019</p>
             <p className="footerCredit">All nutritional data is pulled in from the <a href="https://developer.nutritionix.com/">Nutrionix API.</a></p>
         </footer>
     )
